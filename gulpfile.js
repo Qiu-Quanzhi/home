0aa29f0b8cf075b6fd84e709384dcab1986b4b9c
旅禾Tristan <i@qqzhi.cc>
2025-01-19 14:27:25 +0800

    优化 gulpfile.js，移除 rev 和 revRewrite 相关代码，简化构建任务

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,8 @@
-import { readFileSync } from 'node:fs';
 import gulp from 'gulp';
-import rev from 'gulp-rev';
-import revRewrite from 'gulp-rev-rewrite';
 import cleanCSS from 'gulp-clean-css';
 import htmlmin from 'gulp-html-minifier-terser'
 import htmlclean from 'gulp-htmlclean'
 import terser from 'gulp-terser';
-import revdel from 'gulp-rev-delete-original'
 // 压缩js
 gulp.task('minify-js', () =>
     gulp.src(['./public/**/*.js']) //, '!./public/**/*.min.js'
@@ -16,14 +12,6 @@ gulp.task('minify-js', () =>
                 drop_debugger: true
             }
         }))
-        .pipe(rev())
-        .pipe(gulp.dest('public'))
-        .pipe(revdel())
-        .pipe(rev.manifest({
-            path: 'public/rev-manifest.json',
-            base: 'public',
-            merge: true
-        }))
         .pipe(gulp.dest('public'))
 )
 //压缩css
@@ -32,20 +20,11 @@ gulp.task('minify-css', () => {
         .pipe(cleanCSS({
             compatibility: '*'
         }))
-        .pipe(rev())
-        .pipe(gulp.dest('public'))
-        .pipe(revdel())
-        .pipe(rev.manifest({
-            path: 'public/rev-manifest.json',
-            base: 'public',
-            merge: true
-        }))
         .pipe(gulp.dest('public'))
 });
 //压缩html
 gulp.task('minify-html',() => {
     return gulp.src('public/**/*.html')
-        .pipe(revRewrite({ manifest: readFileSync('public/rev-manifest.json') }))
         .pipe(htmlclean())
         .pipe(htmlmin({
             collapseBooleanAttributes: true,
@@ -71,9 +50,6 @@ gulp.task('minify-html',() => {
         .pipe(gulp.dest('public'))
 });
 
-gulp.task('default', gulp.series(
-    gulp.parallel(
-        'minify-js', 'minify-css'
-    ),
-    'minify-html'
+gulp.task('default', gulp.parallel(
+    'minify-js', 'minify-css', 'minify-html'
 ))
