46f3d566e4aad4dc4de9b95ad42956d4fc62033d
Qiu-Quanzhi <i@qqzhi.cc>
2026-07-10 12:12:34 +0800

    删除不必要的 CSS 和 JavaScript 文件，简化主题结构

--- source/js/script.js
+++
@@ -1,8 +0,0 @@
-function hashScrollIntoView() {
-    try {
-        let element = document.querySelector(decodeURI(location.hash));
-        element.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
-    } catch (error) { }
-}
-window.addEventListener('load', hashScrollIntoView);
-window.addEventListener('hashchange', hashScrollIntoView);
